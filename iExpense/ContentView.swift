56c06cff33166be62f7746ccb633fa2dbd64b2fb
Muhammad Naveed <muhammad.naveed@alliedc.com>
2024-08-19 20:17:56 +0500

    add bottom sheet & Environment for dismiss action

--- iExpense/ContentView.swift
+++ iExpense/ContentView.swift
@@ -7,15 +7,47 @@
 
 import SwiftUI
 
+@Observable
+class User {
+    var firstName = "Bilbo"
+    var lastName = "Baggins"
+}
+
 struct ContentView: View {
+    @State private var user = User()
+    @State private var showSheet = false
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+            Text("Your name is \(user.firstName) \(user.lastName).")
+            
+            TextField("First name", text: $user.firstName)
+            TextField("Last name", text: $user.lastName)
+            Button("OpenSheet") {
+                showSheet.toggle()
+            }
         }
-        .padding()
+        .sheet(isPresented: $showSheet) {
+            BottomSheet(name: "Bottom Sheet Properties")
+        }
+    }
+}
+
+struct BottomSheet: View {
+    let name :String
+    
+    @Environment(\.dismiss) var dismiss
+    
+    var body: some View {
+        ZStack{
+            Color.red
+            VStack{
+                Text(name)
+                Button("Dismiss") {
+                    dismiss()
+                }
+            }
+        }
+        .ignoresSafeArea()
     }
 }
 
