b7696370df308f73c60ef18758788b91434c04b7
Muhammad Naveed <muhammad.naveed@alliedc.com>
2024-08-19 19:22:06 +0500

    Initial Commit

---
+++ iExpense/iExpenseApp.swift
@@ -0,0 +1,17 @@
+//
+//  iExpenseApp.swift
+//  iExpense
+//
+//  Created by Naveed on 19/08/2024.
+//
+
+import SwiftUI
+
+@main
+struct iExpenseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
